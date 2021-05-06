0a9b8166da093160d59c89ceab316bd9d2d506e1
Andres <afsotos@gmail.com>
2021-05-06 18:59:05 -0500

    Cap 28 Estable

--- frontend/src/components/CallCenter.js
+++ frontend/src/components/CallCenter.js
@@ -1,9 +1,11 @@
 import NavBar from './NavBar';
+import CallProgress from './CallProgess';
 
 function CallCenter(){
     return (
         <div>
             <NavBar />
+            <CallProgress />
         </div>
     );
 }
